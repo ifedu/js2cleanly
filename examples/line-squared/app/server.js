e78a43af98bacfab1342e012f523d00fac0e872e
Eduardo Pérez-Bermejo <ifedu@outlook.com>
2014-07-29 22:13:29 -0500

    0.1.4

--- examples/line-squared/app/server.js
+++
@@ -1,6 +0,0 @@
-var jsCleanly = require('js2cleanly.js');
-
-jsCleanly('controller/gameox.js', 'controller/gameox.js-cleanly');
-jsCleanly('main.js', 'main.js-cleanly');
-jsCleanly('directive/any-in-line.js', 'directive/any-in-line.js-cleanly');
-jsCleanly('service/three-in-ray.js', 'service/three-in-ray.js-cleanly');
